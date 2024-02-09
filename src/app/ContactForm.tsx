6f06efa4bea13df8674c99b1f1ca85e972462de4
Ayush Lal <ayush.lal@outlook.com>
2024-02-09 20:15:10 +1000

    adding form

---
+++ src/app/ContactForm.tsx
@@ -0,0 +1,88 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+function ContactForm() {
+  const formSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>CONTACT</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input placeholder="shadcn" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public display name.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
+            {/* This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers. */}
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default ContactForm;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,29 +10,19 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import ContactForm from "./ContactForm";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full flex flex-col justify-center items-center space-y-4">
+    <main className="h-screen w-full flex flex-col justify-center items-center space-y-4 bg-background">
       {/* <ModeToggle /> */}
 
-      <h1>Hi, I'm Ayush.</h1>
-      <h1>I am a Full Stack Software Developer.</h1>
+      <div className="text-center">
+        <h1 className="text-4xl">Hi, I'm Ayush.</h1>
+        <h1 className="text-3xl">I am a Full Stack Software Developer.</h1>
+      </div>
 
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button>Click me</Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+      <ContactForm />
     </main>
   );
 }
