0fd7fe652798580b99fac14b7a3277d824bd22e6
Ayush Lal <ayush.lal@outlook.com>
2024-02-09 15:47:31 +1000

    adding dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,37 @@
 
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <ModeToggle />
+    <main className="h-screen w-full flex flex-col justify-center items-center space-y-4">
+      {/* <ModeToggle /> */}
 
-        <Button>Click me</Button>
-      </div>
+      <h1>Hi, I'm Ayush.</h1>
+      <h1>I am a Full Stack Software Developer.</h1>
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button>Click me</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </main>
   );
 }
